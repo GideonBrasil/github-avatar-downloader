97b8f49a51505468da481e03926efc5e9e798e5b
Gideon Brasil <gideonbrasil@Gideons-MacBook-Air.local>
2019-02-11 19:58:39 -0700

    added request inside get repo function

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,17 @@
 var request = require('request');
 
+// request github API for list of contributers
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
+  
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
 
 console.log('Welcome to the GitHub Avatar Downloader!');
-
