da94aefc83bf8688143818b702afc1503b158f0e
GideonBrasil <gidxx_z@yahoo.com>
2019-02-12 06:11:36 +0000

    connected function

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,10 @@
 var request = require('request');
 var secrets = require('./secrets');
+var fs = require('fs');
+var owner = process.argv[2];
+var repo = process.argv[3];
+
+console.log('Welcome to the GitHub Avatar Downloader!');
 
 // request github API for list of contributers
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -13,20 +18,37 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
   request(apiEndPoint, function(err, res, body) {
     var parsedBody = JSON.parse(body);
-		cb(err, parsedBody);
+    cb(err, parsedBody);
   });
 	
-			
 }
-  
+//download image by URL
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function(response) {
+      console.log('Downloading images yeah!');
+    })
+    .pipe(fs.createWriteStream(filePath));
+}
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-	for (var i = 0; i < result.length; i++) {
-		console.log(result[i].avatar_url);
-		// console.log("Errors:", err);
-		// console.log("Result:", result);
+//check to see if owner and repo match
+if (owner && repo) {
+getRepoContributors(owner, repo, function(err, result) {
+	if (err){
+		console.log('err:', err)
 	}
-
+  for (var contributers = 0; contributers < result.length; contributers++) {
+		var filepath = `./avatars/${contributers.login}.jpg`;
+		var contURL = contributers.avatar_url;
+		downloadImageByURL(contURL, filepath)
+	}
 });
-
-console.log('Welcome to the GitHub Avatar Downloader!');
+} else {
+	console.log('Enter owner and repo you fool.')
+}
+  //  console.log(result[i].avatar_url);
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
