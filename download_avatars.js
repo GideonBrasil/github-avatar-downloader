3029da247682c91265da6c4d2a8da25f978da6d5
GideonBrasil <gidxx_z@yahoo.com>
2019-02-12 04:05:14 +0000

    finish step7 for the avatar project

--- download_avatars.js
+++ download_avatars.js
@@ -7,19 +7,26 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var apiEndPoint = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-			'User-Agent': 'request',
-			'Authorization': 'secrets'
+      'User-Agent': 'request',
+      'Authorization': secrets
     }
   };
-  request(apiEndPoint, cb) 
+  request(apiEndPoint, function(err, res, body) {
+    var parsedBody = JSON.parse(body);
+		cb(err, parsedBody);
+  });
 	
 			
 }
   
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+	for (var i = 0; i < result.length; i++) {
+		console.log(result[i].avatar_url);
+		// console.log("Errors:", err);
+		// console.log("Result:", result);
+	}
+
 });
 
 console.log('Welcome to the GitHub Avatar Downloader!');
