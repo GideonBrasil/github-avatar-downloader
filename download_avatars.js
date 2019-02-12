7d93f9dabfa3c8b7818ab762338c8c4fbde0ffad
Gideon Brasil <gideonbrasil@Gideons-MacBook-Air.local>
2019-02-11 20:50:59 -0700

    step7 for avatar project

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,19 @@
 var request = require('request');
+var secrets = require('./secrets');
 
 // request github API for list of contributers
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
-    cb(err, body);
-  });
+  // fetch the list of contributers from "https://api.github.com/repos/"
+  var apiEndPoint = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+			'User-Agent': 'request',
+			'Authorization': 'secrets'
+    }
+  };
+  request(apiEndPoint, cb) 
+	
+			
 }
   
 
